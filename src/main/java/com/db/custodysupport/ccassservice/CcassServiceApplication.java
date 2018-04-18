93d708b9894f8e3cd8f541e6fce4776bf9721427
Mikhail Shaulskiy <mshaulskiy@gmail.com>
2018-04-18 15:59:50 +0100

    Initial commit

---
+++ src/main/java/com/db/custodysupport/ccassservice/CcassServiceApplication.java
@@ -0,0 +1,12 @@
+package com.db.custodysupport.ccassservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcassServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcassServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/db/custodysupport/ccassservice/CcassServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.db.custodysupport.ccassservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CcassServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
